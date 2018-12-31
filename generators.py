3cee5a04f4074811a865370c6ef52674f0fbe3f0
dominik <dominikpeter@users.noreply.github.com>
2018-12-31 23:20:58 +0100

    Update generators.py

--- generators.py
+++ generators.py
@@ -15,3 +15,33 @@ def to_percent(x):
      
 #chain = to_percent(negative(square(range(1, 10))))
 #list(chain)
+
+#timeit
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        import datetime
+        enter = datetime.datetime.now()
+        result = func(*args, **kwargs)
+        exit = datetime.datetime.now()
+        time = (exit - enter).total_seconds()
+        print(f"Total time = {time}")
+        return result
+    return wrapper
+
+@timeit
+def random_loop(n):
+    import numpy as np
+    x = np.zeros(n)
+    for i in range(n):
+        x[i] = np.random.randint(0, 100, n).mean()
+    return x.mean()
+
+#random_loop(100)
+#random_loop(10000)
+
+
+
+
+
+
+
