6d90b43b525ff209a49b22ccc5d4f5fee095cfdc
dominik <dominikpeter@users.noreply.github.com>
2018-12-31 21:13:56 +0100

    Create generators.py

---
+++ generators.py
@@ -0,0 +1,17 @@
+# chain decorators
+
+def square(x):
+    for i in x:
+      yield i**2
+     
+def negative(x):
+    for i in x:
+        yield abs(i)*-1
+        
+def to_percent(x):
+    for i in x:
+        string = str(i) + " %"
+        yield string
+     
+#chain = to_percent(negative(square(range(1, 10))))
+#list(chain)
