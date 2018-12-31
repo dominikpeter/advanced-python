705e39e0651a03f35d59ef5403a6b5bf5fd5d236
dominik <dominikpeter@users.noreply.github.com>
2018-12-31 23:23:37 +0100

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -51,3 +51,31 @@ def string_multiplier(string, n):
     
     
 #string_multiplier("HALLO", 2)
+
+
+
+#timeit
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        import datetime
+        enter = datetime.datetime.now()
+        result = func(*args, **kwargs)
+        exit = datetime.datetime.now()
+        time = (exit - enter).total_seconds()
+        print(f"Total time = {time}")
+        return result
+    return wrapper
+
+@timeit
+def random_loop(n):
+    import numpy as np
+    x = np.zeros(n)
+    for i in range(n):
+        x[i] = np.random.randint(0, 100, n).mean()
+    return x.mean()
+
+#random_loop(100)
+#random_loop(10000)
+
+
+
